e49d97f820ed32b7273a2f618abcbbb1fa7ccf00
warband0 <55248186+warband0@users.noreply.github.com>
2022-01-24 21:08:35 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    let array1 = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"];
+    let array2 = ["","ten","twenty","thirty","forty",'fifty','sixty','seventy','eighty','ninety']
+    let a =  number.toString().split('')
+    if (number / 10 <= 2) {
+      return array1[number]
+    } else if (number / 10 > 2 && number / 10 < 10) {
+        if (a[1] != "0") {return array2[Number(a[0])] +" " + array1[Number(a[1])]}
+        else {return array2[Number(a[0])]}
+    } else if (number / 10 >= 10) {
+        if (a[1] == "0" && a[2] == "0") {
+          return array1[Number(a[0])] + " " + "hundred"
+      } else if (a[1] == "0" && a[2] != "0") {
+          return array1[Number(a[0])] + " " + "hundred" + " " + array1[Number(a[2])]
+      } else if (a[1] != "0" && a[2] == "0"){
+          return array1[Number(a[0])] + " " + "hundred" + " " + array2[Number(a[1])]
+      } else if (a[1] != "0" && Number(a[1]) < 2 && a[2] != "0") {
+            return array1[Number(a[0])] + " " + "hundred" + " " + array1[Number(a[1] + a[2])]
+         }
+       else  {
+         return array1[Number(a[0])] + " " + "hundred" + " " + array2[Number(a[1])] + " " + array1[Number(a[2])]
+      }
+    }
 }
